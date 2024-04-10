01f02b20739dedcefdd9749f506da7af50ed71e9
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-04-11 00:11:40 +0530

    Update Task module added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app= express();
 require("./conn/conn");
 const auth = require("./routes/auth");
+const list = require("./routes/list");
 app.use(express.json());
 
 app.get("/",(req,res)=>
@@ -12,6 +13,7 @@ res.send("Hello");
 });
 
 app.use("/api/v1",auth);
+app.use("/api/v2",list);
 
 app.listen(1000,()=>{
 
