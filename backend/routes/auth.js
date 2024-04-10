0f4ee05dea4234ff3e83c698f77b1f20ec200f9f
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-04-10 23:33:36 +0530

    Sign in block intergrated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -28,5 +28,35 @@ router.post("/register", async (req, res) => {
 //Sign-IN
 
 
+router.post("/signin", async (req, res) => {
+    try {
+      
+      const user  = await User.findOne({ email: req.body.email });
+
+      if(!user)
+      {
+
+        res.status(400).json({ message: "Please Sign Up First" }); 
+
+      }
+
+      const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
+
+      if(!isPasswordCorrect)
+      {
+
+        res.status(400).json({ message: "Password is not correct" }); 
+
+      }
+
+      const {password, ...others} = user._doc;
+      res.status(200).json({ others });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" }); // Generic error response
+    }
+});
+
+
 
 module.exports = router;
