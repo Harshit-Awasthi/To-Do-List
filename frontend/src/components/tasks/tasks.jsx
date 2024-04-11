900d82fa6275ffb4318cbacd1d194f72c1f04d6c
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-04-11 17:00:37 +0530

    Added: Task Cards

--- frontend/src/components/tasks/tasks.jsx
+++ frontend/src/components/tasks/tasks.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tasks.css'
+import TasksCards from './tasksCards';
 
 
 const Tasks = () => {
+
+    const [Inputs, setInputs]= useState({title:"", body:""});
+    const [Array, setArray]= useState([]);
+
+
 
 const show = ()=>{
 
@@ -10,6 +16,20 @@ const show = ()=>{
 
 
 }
+
+const change = (e)=>{
+
+    const {name,value} = e.target;
+    setInputs({...Inputs,[name]:value})
+
+}
+
+const submit = ()=>{
+
+    setArray([...Array,Inputs]);
+    setInputs({title: "", body: ""});
+
+};
 
   return (
   
@@ -20,25 +40,59 @@ const show = ()=>{
         <div className='d-flex flex-column tasks-inputs-div w-50 p-1'>
 
         <input className='my-2 p-2 tasks-inputs'  type="text" placeholder='Title'
-        onClick={show}  />
+        onClick={show} 
+        name="title"
+        value={Inputs.title}
+        onChange={change}
+        
+        />
 
-        <textarea id="textarea" className='p-2 tasks-inputs' type="text" placeholder='Body'   />
+        
+
+        <textarea id="textarea" className='p-2 tasks-inputs' type="text" placeholder='Body' 
+         name="body" 
+         value={Inputs.body}
+        onChange={change}
+        />
 
         </div>
 
         <div className='w-50 d-flex justify-content-end my-3'>
 
-        <button className='home-btn px-2 py-1'>Add</button>
+        <button className='home-btn px-2 py-1' onClick={submit}>Add</button>
 
         </div>
 
 
     </div>
   
+    <div className="tasks-body">
+
+    <div className="container-fluid"> 
+
+    <div className='row '>
+
+    {Array && Array.map((item,index)=>
+    (
+    
+    <div className='col-lg-3 col-10 mx-5 my-2'>
+
+    <TasksCards title={item.title} body={item.body} />
+    
+    </div>
+    
+    ))}
+
+    </div>
+    
+     </div>
+
+    </div>
+
   </div>
   
 );
 
-}
+};
 
 export default Tasks;
---
+++ frontend/src/components/tasks/tasksCards.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaDeleteLeft } from "react-icons/fa6";
+import { MdOutlineUpdate } from "react-icons/md";
+
+
+const TasksCards = ({title,body} ) => {
+  return (
+  
+  <div className='p-3 tasks-card'>
+ 
+    <div>
+      
+      <h5> {title} </h5>
+      <p className='tasks-card-p'>
+
+      {body.split("",77)}...
+
+      </p>
+      
+      </div>
+
+      <div className='d-flex justify-content-around'>
+
+        <div className='d-flex justify-content-center align-items-center card-icon-head px-2 py-1'>
+        <MdOutlineUpdate  className='card-icons' /> Update
+
+        </div>
+
+      <div className='d-flex justify-content-center align-items-center card-icon-head px-2 py-1 text-danger'>
+
+      <FaDeleteLeft className='card-icons del' /> Delete
+
+      </div>
+
+      </div>
+  
+  </div>
+  
+);
+
+}
+
+export default TasksCards;
