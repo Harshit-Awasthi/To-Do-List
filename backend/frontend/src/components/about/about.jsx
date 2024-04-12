25c5d73e8292fc204868b0e30b4e4ba0a55072a1
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-04-12 18:18:55 +0530

    Deployment

---
+++ backend/frontend/src/components/about/about.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+  
+  <div className='about d-flex justify-content-center align-items-center'>
+    
+    <div className="container">
+
+        <div className='d-flex'>
+
+        <h1>About Us</h1>
+
+        </div>
+
+  
+
+<p>
+We were tired of to-do lists that felt overwhelming or restrictive. We craved an app that was flexible, adaptable, and fit seamlessly into our lives. So, we created Task Whiz.
+
+<br />
+
+Task Whizis more than just a checklist. It's a powerful tool designed to help you.
+
+<br />
+<br />
+<br />
+
+
+The Task Whiz team is passionate about helping people achieve their personal and professional aspirations. We believe that by getting organized, we can all unlock our full potential. Join us on this journey to productivity and let's get things done together!
+</p>
+
+    </div>
+
+
+  
+  </div>
+  
+);
+
+}
+
+export default About;
---
+++ backend/frontend/src/components/home/home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+  
+  <div className='home d-flex justify-content-center align-items-center'>
+    
+ <div className="container d-flex justify-content-center align-items-center flex-column">
+
+    <h1 className='text-center'> Streamline your <br/> daily tasks with ease. </h1>
+    <br/>
+    <br/>
+
+    <p>
+
+    Achieve clarity, structure, and serenity using  Task Whiz, <br/>your ultimate solution for staying on top of your to-dos.
+
+    </p>
+
+ </div>
+  
+  </div>
+  
+);
+
+}
+
+export default Home;
