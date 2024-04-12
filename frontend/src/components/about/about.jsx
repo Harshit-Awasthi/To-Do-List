1ce29b6cf1420f65cc1326b1a16cf6ff2bacd9e7
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-04-12 16:07:51 +0530

    Updated: UI

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -17,12 +17,18 @@ const About = () => {
   
 
 <p>
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+We were tired of to-do lists that felt overwhelming or restrictive. We craved an app that was flexible, adaptable, and fit seamlessly into our lives. So, we created Task Whiz.
 
 <br />
 
+Task Whizis more than just a checklist. It's a powerful tool designed to help you.
 
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+<br />
+<br />
+<br />
+
+
+The Task Whiz team is passionate about helping people achieve their personal and professional aspirations. We believe that by getting organized, we can all unlock our full potential. Join us on this journey to productivity and let's get things done together!
 </p>
 
     </div>
--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -9,18 +9,14 @@ const Home = () => {
  <div className="container d-flex justify-content-center align-items-center flex-column">
 
     <h1 className='text-center'> Streamline your <br/> daily tasks with ease. </h1>
+    <br/>
+    <br/>
 
     <p>
 
-    Achieve clarity, structure, and serenity using  Task Whiz <br/>—your ultimate solution for staying on top of your to-dos.
+    Achieve clarity, structure, and serenity using  Task Whiz, <br/>your ultimate solution for staying on top of your to-dos.
 
     </p>
-
-    <button className="home-btn p-2" >
-
-    Make ToDo List
-
-    </button>
 
  </div>
   
